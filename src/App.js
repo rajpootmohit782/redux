f3e8f9826170d78258419fb35c1c3b12a4d68f20
rajpootmohit782 <84062871+rajpootmohit782@users.noreply.github.com>
2023-01-19 12:39:35 +0530

    t12

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import "./styles.css";
+import Counter from './components/Counter';
 
-export default function App() {
-  const redux = require("redux");
 
-  const counterReducer = (state = { counter: 0 }, action) => {
-    if (action.type === "increment") {
-      return {
-        counter: state.counter + 1
-      };
-    }
-    if (action.type === "decrement") {
-      return {
-        counter: state.counter - 1
-      };
-    }
-  };
-  const store = redux.createStore(counterReducer);
-
-  const counterSubscriber = () => {
-    const latestate = store.getState();
-    console.log(latestate);
-  };
-
-  //store.subscribe(counterSubscriber);
-
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "increment" });
-  store.dispatch({ type: "decrement" });
-
-  counterSubscriber();
+function App() {
   return (
-    <div className="App">
-      <h1>kuch</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Counter />
   );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import { Provider } from "react-redux";
+
+import store from "./store/ind";
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
